16b5a7ecdc2491c624c0079d4293474a02f2f344
Nutmeg <emjayess@gmail.com>
2017-09-07 07:36:46 -0600

    code

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,31 @@
+function code() {
+    const code = [38,
+        38,
+        40,
+        40,
+        37,
+        39,
+        37,
+        39,
+        66,
+        65];
+    
+    let index = 0;
+
+    $(document).keydown(function(key) {
+        if (key.which === code[index]) {
+            index++;
+            if (index === code.length) {
+                alert(`KONAMI`);
+                index = 0;
+            }
+        } else {
+            index = 0;
+        }
+    });
+
+}
+
 $(document).ready(function() {
-    console.log(`ready`);
+    code();
 });
